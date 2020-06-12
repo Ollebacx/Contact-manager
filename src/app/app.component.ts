84d6bb917807d71ab42acadd86a86b95933f8c43
aisha-125 <aisyanez.1250@gmail.com>
2020-06-12 11:48:10 +0100

    Checkemail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'contact-manager';
@@ -16,27 +16,38 @@ export class AppComponent {
   };
   public error: boolean = false;
   public nameError: string = '';
-  saveContact(contact : any){
-    if (this.contact.name.length > 6){
-    this.contactList.push(contact);
+  public emailError: string = '';
+  
 
-    this.contact = {
-      name: '',
-      lastname: '',
-      email: '',
-      telephone: null,
-    } 
+  saveContact(contact: any) {
+    if (
+      this.contact.name.length > 6 &&
+      !this.contactList.some((contact) => contact.email == this.contact.email)
+    ) {
+      this.contactList.push(contact);
+      this.contact = {
+        name: '',
+        lastname: '',
+        email: '',
+        telephone: null,
+      };
+    }
   }
 
-  }
-  onBlur(){
-    if (this.contact.name.length < 6){
+  onBlur() {
+    if (this.contact.name.length < 6) {
       this.nameError = 'Tu nombre tiene que tener más de 6 caracteres.';
       this.error = true;
     }
-  }
-  onFocus(){
-      this.nameError = '';
-      this.error = false;
+    if (this.contactList.some((contact) => contact.email === this.contact.email)) {
+      this.emailError = 'Este correo ya esta registrado';
+      this.error = true;
     }
+    console.log(this.contactList.some((contact) => contact.email === this.contact.email))
   }
+  onFocus() {
+    this.nameError = '';
+    this.emailError = '';
+    this.error = false;
+  }
+}
